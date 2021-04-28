ab8d123bfb2452783c5f8757888345c1fa038bd2
Sebastian <sebastian.r.s3107@hotmail.com>
2021-04-28 14:59:37 +0200

    Initial commit

---
+++ app/src/main/java/com/sebrs3018/login/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sebrs3018.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
